908e7e7516d17f95c565cf9b42c984fae9c14ddc
MEGHWANT SINGH <meghwantsingh8427@gmail.com>
2022-09-25 12:28:15 +0530

    Virtual internship kotlin work
    Done by Meghwant Singh
    google profile : https://developers.google.com/profile/u/104096253742773573210

---
+++ Create a birthday message in Kotin/birthday.kt
@@ -0,0 +1,21 @@
+fun main() {
+    
+    val age = 20 
+    val name = "Meghwant"
+    
+    println("Happy Birthday, ${name}!")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age} year old, ${name}!")
+    println("${age} year old is the very best age to celebrate!")
+}
